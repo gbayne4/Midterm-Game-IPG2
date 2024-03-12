1c824dffed5714b1285880f109ffe65b601dac8c
gillian <gvb9858@nyu.edu>
2024-03-11 22:35:46 -0400

    Update MakeSnowmanHelper.cs

--- Assets/Scripts/MakeSnowmanHelper.cs
+++ Assets/Scripts/MakeSnowmanHelper.cs
@@ -13,7 +13,7 @@ public class MakeSnowmanHelper : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        hatsCollected = 0;
     }
 
     // Update is called once per frame
